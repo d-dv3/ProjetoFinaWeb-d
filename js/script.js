ae82a469a4a710f71d2e7aab7c6d2a7dc383dfa2
d-dv3 <diana.doris@gmail.com>
2026-03-27 20:35:44 +0000

    form-random color-enterEvent

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@
 
 //
 //FORM
-// let prtForm = document.getElementById("portflForm");
-// let myNome = document.getElementById("newNamePlaceholder");
-// let myFrase = document.getElementById("novaFrasePlaceholder");
-// let newImgUrl = document.getElementById("trocarImg");
+let prtForm = document.getElementById("portflForm");
+let myNome = document.getElementById("newNamePlaceholder");
+let myFrase = document.getElementById("novaFrasePlaceholder");
+let newImgUrl = document.getElementById("trocarImg");
 
-// prtForm.addEventListener("submit", function (event) {
-//   event.preventDefault();
-//   let prfData = new FormData(this);
+prtForm.addEventListener("submit", function (event) {
+  event.preventDefault();
+  let prfData = new FormData(this);
 
-//   myNome.innerText = prfData.get("name");
-//   myFrase.innerText = prfData.get("frase");
-//   newImgUrl.setAttribute("src", prfData.get("foto"));
+  myNome.innerText = prfData.get("name");
+  myFrase.innerText = prfData.get("frase");
+  newImgUrl.setAttribute("src", prfData.get("foto"));
 
-//   this.reset();
-// });
+  this.reset();
+});
 //
 // API
 let askJokeBtn = document.getElementById("getJoke");
@@ -33,4 +33,36 @@ askJokeBtn.addEventListener("click", function () {
 
       jokeList.appendChild(li);
     });
+});
+//
+//ENTER EVENT
+document.addEventListener("keydown", function (event) {
+  console.log(event.key);
+  if (event.key == "Enter") {
+    alert("evento de teclado!" + event.key);
+  }
+});
+//
+//
+// RANDOM COLORS
+function getRandomBKGcolor() {
+  return Math.floor(Math.random() * 255);
+}
+//
+let randomColorBtn = document.getElementById("corAbtn");
+let colorsSpot = document.getElementById("allColors");
+
+randomColorBtn.addEventListener("click", () => {
+  let r = getRandomBKGcolor();
+  let g = getRandomBKGcolor();
+  let b = getRandomBKGcolor();
+  console.log(r);
+  console.log(g);
+  console.log(b);
+
+  let rColor = `rgb( ${r}, ${g}, ${b} )`;
+  console.log(`rgb( ${r}, ${g}, ${b} )`);
+  console.log(rColor);
+
+  colorsSpot.style.backgroundColor = rColor;
 });
